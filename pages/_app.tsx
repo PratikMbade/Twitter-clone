96cafbfe14f855daeefb174beebc5ababf9acb46
PratikMbade <pratikmbade18@gmail.com>
2023-08-17 23:42:39 +0530

    google-auth-from-scratch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import '@/styles/globals.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
-
+import {Toaster} from 'react-hot-toast'
 
 
 const inter = Inter({ subsets: ['latin'] })
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className={inter.className}> <Component {...pageProps} /> </div> 
+  return(
+     <div className={inter.className}>
+      <GoogleOAuthProvider clientId='621021601569-okml78fbms7rc3p8flmjih11gdnkn36h.apps.googleusercontent.com'>
+      <Toaster  position="top-left"
+             reverseOrder={false}/>
+         <Component {...pageProps} />
+      
+      </GoogleOAuthProvider>
+     </div> 
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,150 @@
-import {BsTwitter} from 'react-icons/bs'
-import {RiHome7Fill} from 'react-icons/ri'
-import {FiSearch} from 'react-icons/fi'
-import {PiBellBold} from 'react-icons/pi'
-import {MdMailOutline} from 'react-icons/md'
-import {BiBookmark} from 'react-icons/bi'
-import {HiOutlineUser,HiOutlineUsers} from 'react-icons/hi'
-import {CgMoreO} from 'react-icons/cg'
-import {RiBook2Line} from 'react-icons/ri'
+import { BsTwitter } from "react-icons/bs";
+import { RiHome7Fill } from "react-icons/ri";
+import { FiSearch } from "react-icons/fi";
+import { PiBellBold } from "react-icons/pi";
+import { MdMailOutline } from "react-icons/md";
+import { BiBookmark } from "react-icons/bi";
+import { HiOutlineUser, HiOutlineUsers } from "react-icons/hi";
+import { CgMoreO } from "react-icons/cg";
+import { RiBook2Line } from "react-icons/ri";
 
-
-
-import { FeedCard } from '@/components/FeedCard'
-
-
-
+import { FeedCard } from "@/components/FeedCard";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { useCallback } from "react";
+import toast from "react-hot-toast/headless";
+import { graphqlClient } from "@/clients/api";
+import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
 
 interface TwitterSidebarButton {
-  id:number,
+  id: number;
   title: String;
-  icon:React.ReactNode;
+  icon: React.ReactNode;
 }
-
 
 const sidebarMenuItems: TwitterSidebarButton[] = [
   {
-    id:1,
-    title:"Home",
-    icon:<RiHome7Fill/>
+    id: 1,
+    title: "Home",
+    icon: <RiHome7Fill />,
   },
   {
-    id:2,
-    title:"Explore",
-    icon:<FiSearch/>
+    id: 2,
+    title: "Explore",
+    icon: <FiSearch />,
   },
   {
-    id:3,
-    title:"Notifications",
-    icon:<PiBellBold/>
+    id: 3,
+    title: "Notifications",
+    icon: <PiBellBold />,
   },
   {
-    id:4,
-    title:"Messages",
-    icon:<MdMailOutline/>
+    id: 4,
+    title: "Messages",
+    icon: <MdMailOutline />,
   },
   {
-  id:5,
-  title:"Lists",
-  icon:<RiBook2Line/>
+    id: 5,
+    title: "Lists",
+    icon: <RiBook2Line />,
   },
   {
-    id:6,
-    title:"Bookmarks",
-    icon:<BiBookmark/>
+    id: 6,
+    title: "Bookmarks",
+    icon: <BiBookmark />,
   },
   {
-    id:7,
-    title:"Profile",
-    icon:<HiOutlineUser/>
+    id: 7,
+    title: "Profile",
+    icon: <HiOutlineUser />,
   },
   {
-    id:8,
-    title:"Communities",
-    icon:<HiOutlineUsers/>
+    id: 8,
+    title: "Communities",
+    icon: <HiOutlineUsers />,
   },
   {
-    id:8,
-    title:"More",
-    icon:<CgMoreO/>
-  }
-]
+    id: 9,
+    title: "More",
+    icon: <CgMoreO />,
+  },
+];
 
 export default function Home() {
+ 
+  const handleLoginWithGoogle = useCallback( async (cred:CredentialResponse) => {
+    const googleToken = cred.credential
+
+    if(!googleToken) return toast.error(`google token is not found`)
+
+    const {verifyGoogleToken} = await graphqlClient.request(verifyUserGoogleTokenQuery,
+      {token:googleToken}
+      )
+
+      
+      console.log(verifyGoogleToken)
+    
+      if(verifyGoogleToken){
+        toast.success("Verify Successfully")
+        window.localStorage.setItem("__twitter_token",verifyGoogleToken)
+      }
+
+      
+  },
+[])
+
   return (
-     <div>
-        <div className='grid grid-cols-12 h-screen w-screen px-52'>
-          <div className='col-span-3 pt-2 ml-32 '>
-            <div className='text-3xl  hover:bg-gray-800 mb-2 rounded-full p-2 cursor-pointer transition-all w-fit'>
-            <BsTwitter className="text-3xl "/>
-            </div>
-            <div >
-              <ul >
-                {sidebarMenuItems.map((item) =>{
-                  return <li key={item.id} className='flex font-light  items-center justify-center text-2xl px-3 py-2 pl-2 hover:bg-zinc-800 rounded-full  w-fit cursor-pointer transition-all my-2 '>
-                       <span className='text-3xl'>{item.icon}</span>
-                       <span className='pl-4 text-xl '>{item.title}</span>
+    <div>
+      <div className="grid grid-cols-12 h-screen w-screen px-52">
+        <div className="col-span-3 pt-2 ml-32 ">
+          <div className="text-3xl  hover:bg-gray-800 mb-2 rounded-full p-2 cursor-pointer transition-all w-fit">
+            <BsTwitter className="text-3xl " />
+          </div>
+          <div>
+            <ul>
+              {sidebarMenuItems.map((item) => {
+                return (
+                  <li
+                    key={item.id}
+                    className="flex font-light  items-center justify-center text-2xl px-3 py-2 pl-2 hover:bg-zinc-800 rounded-full  w-fit cursor-pointer transition-all my-2 "
+                  >
+                    <span className="text-3xl">{item.icon}</span>
+                    <span className="pl-4 text-xl ">{item.title}</span>
                   </li>
-                })}
-              </ul>
-              <button className='bg-[#1d9bf0] mt-4  rounded-full text-xl font-bold px-24 py-3 hover:bg-[#2488ca] transition-all'>Post</button>
-            </div>
+                );
+              })}
+            </ul>
+            <button className="bg-[#1d9bf0] mt-4  rounded-full text-xl font-bold px-24 py-3 hover:bg-[#2488ca] transition-all">
+              Post
+            </button>
           </div>
-          <div className='col-span-5 border-x border-x-slate-800 ml-3 overflow-y-scroll no-scrollbar scroll-smooth'>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-            <FeedCard/>
-          </div>
-          <div className='col-span-4 '> </div>
         </div>
-     </div>
-  )
+        <div className="col-span-5 border-x border-x-slate-800 ml-3 overflow-y-scroll no-scrollbar scroll-smooth">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
+        <div className="col-span-4 ">
+          <div>
+            <GoogleLogin onSuccess={handleLoginWithGoogle}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
