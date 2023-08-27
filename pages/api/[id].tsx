7858c5d2a98f0b930daaad47d963a894033902fc
PratikMbade <pratikmbade18@gmail.com>
2023-08-27 23:53:19 +0530

    user-profile

--- pages/api/[id].tsx
+++ pages/api/[id].tsx
@@ -1,13 +1,25 @@
 import Twitterlayout from '@/components/FeedCard/Layout/TwitterLayout';
 import type {NextPage} from 'next';
+import { BsArrowLeftShort } from 'react-icons/bs';
 
 
 const UserProfilePage: NextPage = () =>{
     return (
         <div>
             <Twitterlayout>
-                <h1>Profile Page</h1>
+                <div>
+                    <nav className='border flex items-center'>
+                        <BsArrowLeftShort className="border flex items-center"/>
+                        <div>
+                            <h1 className='text-4xl'>Pratik Bade</h1>
+                            <h1 className='text-4xl'>102 Tweets</h1>
+                        </div>
+                    </nav>
+                </div>
             </Twitterlayout>
         </div>
     )
 }
+
+
+export default UserProfilePage;
